1046f85b88abdf85bb1054867cff494f782dd467
Robart Stoyanov <stoyanovbg@abv.bg>
2023-02-13 23:16:40 +0200

    Init template for the shop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import ListProduct from './components/ListProduct/ListProduct';
+import Product from './components/Product/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <Navigation />
+        </header>
+        <Routes>
+          <Route path='/' element= {<Home />}/>
+          <Route path='/man-collection' element= {<ListProduct />}/>
+          <Route path='/woman-collection' element= {<ListProduct />}/>
+          <Route path='/product/:id' element= {<Product />} />
+          <Route path='/offers' element= {<ListProduct />}/>
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
